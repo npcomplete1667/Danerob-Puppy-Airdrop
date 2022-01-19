ba9320a43e820e3ba774752a13a8540572e42f22
hughjassol <92876567+hughjassol@users.noreply.github.com>
2022-01-19 14:42:09 -0800

    Add files via upload

---
+++ src/MintButton.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import { CandyMachineAccount } from './candy-machine';
+import { CircularProgress } from '@material-ui/core';
+import { GatewayStatus, useGateway } from '@civic/solana-gateway-react';
+import { useEffect, useState } from 'react';
+
+export const CTAButton = styled(Button)`
+  width: 100%;
+  height: 50px;
+  background: #737373;
+  color: black;
+  font-size: 30px;
+  border-radius: 0;
+`; // add your own styles here
+
+export const MintButton = ({
+  onMint,
+  candyMachine,
+  isMinting,
+}: {
+  onMint: () => Promise<void>;
+  candyMachine?: CandyMachineAccount;
+  isMinting: boolean;
+}) => {
+  const { requestGatewayToken, gatewayStatus } = useGateway();
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    if (gatewayStatus === GatewayStatus.ACTIVE && clicked) {
+      onMint();
+      setClicked(false);
+    }
+  }, [gatewayStatus, clicked, setClicked, onMint]);
+
+  return (
+    <CTAButton
+      className="mint-button"
+      disabled={
+        candyMachine?.state.isSoldOut ||
+        isMinting ||
+        !candyMachine?.state.isActive
+      }
+      onClick={async () => {
+        setClicked(true);
+        if (candyMachine?.state.isActive && candyMachine?.state.gatekeeper) {
+          if (gatewayStatus === GatewayStatus.ACTIVE) {
+            setClicked(true);
+          } else {
+            await requestGatewayToken();
+          }
+        } else {
+          await onMint();
+          setClicked(false);
+        }
+      }}
+      variant="contained"
+    >
+      {candyMachine?.state.isSoldOut ? (
+        'SOLD OUT'
+      ) : isMinting ? (
+        <CircularProgress />
+      ) : (
+        'CLAIM'
+      )}
+    </CTAButton>
+  );
+};
